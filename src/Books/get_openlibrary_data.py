0dc061798668e37fd20778f67c6be568b48fef96
Christian Clauss <cclauss@me.com>
2021-11-01 18:58:26 +0100

    Update get_openlibrary_data.py

--- src/Books/get_openlibrary_data.py
+++ src/Books/get_openlibrary_data.py
@@ -27,5 +27,70 @@ def get_openlibrary_data(olid: str) -> dict:
     return requests.get(f"https://openlibrary.org/{new_olid}.json").json()
 
   
-if __name__ == "__main__":
-    print(json.dumps(get_openlibrary_data("isbn/0425016013"), indent=4))
+def build_data_class(data, depth = 1, sort_keys = True) -> None:
+    """
+    Semi-automate the process of building data classes from json data.
+    See docstring at the bottom of the file for output.
+    """
+    indent = ' ' * depth * 4
+    if sort_keys:
+        data = {k: v for k, v in sorted(data.items())}  # sort keys in alphabetic order
+    for key, value in data.items():
+        if key == "type":
+            key = "type_"
+        s = f"{indent}{key}: {type(value).__name__}"  # ocaid: str
+        if value and isinstance(value, list):
+            print(f"{s}[{type(value[0]).__name__}]")  # covers: list[int]
+            if isinstance(value[0], dict):
+                build_data_class(value[0], depth + 1)
+        else:
+            print(s)
+            if value and isinstance(value, dict):
+                build_data_class(value, depth + 1)
+
+
+if __name__ == "__main__":  # https://openlibrary.org/works/OL19545135W.json
+    data = get_openlibrary_data("isbn/0140328726")  # isbn/0425016013")
+    # print(data)
+    # print(json.dumps(data, indent=4))
+    print("\n\nclass Work:")
+    build_data_class(data)
+
+"""
+class Work:
+    authors: list[dict]
+        key: str
+    classifications: dict
+    contributions: list[str]
+    covers: list[int]
+    created: dict
+        type_: str
+        value: str
+    first_sentence: dict
+        type_: str
+        value: str
+    identifiers: dict
+        goodreads: list[str]
+        librarything: list[str]
+    isbn_10: list[str]
+    isbn_13: list[str]
+    key: str
+    languages: list[dict]
+        key: str
+    last_modified: dict
+        type_: str
+        value: str
+    latest_revision: int
+    local_id: list[str]
+    number_of_pages: int
+    ocaid: str
+    publish_date: str
+    publishers: list[str]
+    revision: int
+    source_records: list[str]
+    title: str
+    type_: dict
+        key: str
+    works: list[dict]
+        key: str
+"""
